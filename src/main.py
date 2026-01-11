fa0bd09e79f936c1ab2a96e77d0178a4522e79f9
talhamuhammadali <talha_muhammadali@hotmail.com>
2026-01-11 14:50:02 +0500

    [ops] added pre commit formatter and linter

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """Main module for document processing."""
+
 import torch
-import json
-from docling.document_converter import DocumentConverter
+from docling_core.types.doc.document import DoclingDocument
 
 print("Torch version:", torch.__version__)
 print("Cuda available:", torch.cuda.is_available())
@@ -9,8 +9,12 @@ print("Cuda device count:", torch.cuda.device_count())
 print("Cuda Version:", torch.version.cuda)
 
 if __name__ == "__main__":
-    converter = DocumentConverter()
-    file_path = "data/document.pdf"
-    result = converter.convert(file_path)
-    with open("data/processed_doc.json", "w") as f:
-        f.write(result.document.model_dump_json(indent=4))
+    from pprint import pprint
+
+    print("Exploring docling document type")
+    pprint(DoclingDocument, indent=2)
+    # converter = DocumentConverter()
+    # file_path = "data/document.pdf"
+    # result = converter.convert(file_path)
+    # with open("data/processed_doc.json", "w") as f:
+    #     f.write(result.document.model_dump_json(indent=4))
